1a705f72fc65a8ddbb0b51bf37c7431c30f8f29b
yeon <test2@test2.com>
2024-02-08 16:17:45 +0900

    브랜치

--- src/components/LoginFunc.jsx
+++ src/components/LoginFunc.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 function LoginFunc() {
-  return (
-    <div>
-      
-    </div>
-  )
+	return <div></div>;
 }
 
-export default LoginFunc
+export default LoginFunc;
+//
