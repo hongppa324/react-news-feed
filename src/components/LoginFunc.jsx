df794e23543ae57a13114ea16e119c6c57e3527e
yeon <test2@test2.com>
2024-02-08 16:17:15 +0900

    브랜치 생성

--- src/components/LoginFunc.jsx
+++ src/components/LoginFunc.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 function LoginFunc() {
-  return (
-    <div>
-      
-    </div>
-  )
+	return <div></div>;
 }
 
-export default LoginFunc
+export default LoginFunc;
+
+//
