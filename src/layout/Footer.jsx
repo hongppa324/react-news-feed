e70a495971c2e38f21e8fe47293c05fd05ce0e2c
YeonjinJo <hrj8441@naver.com>
2024-02-12 08:54:30 +0900

    Login

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <StyledHeader>
+      <p>Footer</p>
+    </StyledHeader>
   )
 }
 
 export default Footer
+
+const StyledHeader = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 5vh;
+  background-color: rgba(20, 20, 20, 0.25);
+`;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "../routes/Main";
+import LoginPage from "../routes/LoginPage";
+import SignUpPage from "../routes/SignUpPage";
+import Home from "../routes/Home";
 
-export default function Router() {
-  return <div>Router</div>;
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
 }
+
+export default Router;
