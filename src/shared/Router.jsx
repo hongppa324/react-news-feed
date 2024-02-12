d4d272e59d8103c79566910cd0d3444dc37402d2
yeji Sung <dpwl0332@naver.com>
2024-02-13 02:28:35 +0900

    커밋기록2

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import FeedItem from "../components/crud/FeedItem";
->>>>>>> c19ce06065f0e92660eef6b3e362dc0a24dfff49
 import Main from "../routes/Main";
 import LoginPage from "../routes/LoginPage";
 import SignUpPage from "../routes/SignUpPage";
 import Home from "../routes/Home";
-<<<<<<< HEAD
-import ResetPassword from "../routes/ResetPassword";
-=======
->>>>>>> c19ce06065f0e92660eef6b3e362dc0a24dfff49
-
+import Comment from "../pages/Comment";
 function Router() {
   return (
     <Routes>
@@ -20,13 +13,9 @@ function Router() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/sign-up" element={<SignUpPage />} />
       <Route path="/feedItem" element={<FeedItem />} />
+      <Route path="/comment" element={<Comment />} />
       <Route path="/home" element={<Home />} />
-<<<<<<< HEAD
-      <Route path="/reset" element={<ResetPassword />} />
-=======
->>>>>>> c19ce06065f0e92660eef6b3e362dc0a24dfff49
     </Routes>
   );
 }
-
 export default Router;
